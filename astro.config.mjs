5d4e3720311e647a543ea2b39a03ddebc889f090
Wojciech Zgliniecki <wojciech.zgliniecki@gmail.com>
2024-11-12 14:40:02 +0100

    Added sitemap and robots.txt. Prettified whole project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://wzgliniecki.github.io",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
