ce53c94c2e820ee017e37b0630eb9d3273d3c617
Wojciech Zgliniecki <wojciech.zgliniecki@gmail.com>
2024-11-14 15:29:13 +0100

    Basic color palette changed. Development Warning removed. Tailwind theme added.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,44 @@
+// import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#88aaee',
+        mainAccent: '#4d80e6', // not needed for shadcn components
+        overlay: 'rgba(0,0,0,0.8)', // background color overlay for alert dialogs, modals, etc.
+  
+        // light mode
+        bg: '#dfe5f2',
+        text: '#000',
+        border: '#000',
+  
+        // dark mode
+        darkBg: '#272933',
+        darkText: '#eeefe9',
+        darkBorder: '#000',
+        secondaryBlack: '#212121', // opposite of plain white, not used pitch black because borders and box-shadows are that color 
+      },
+      borderRadius: {
+        base: '19px'
+      },
+      boxShadow: {
+        light: '6px 6px 0px 0px #000',
+        dark: '6px 6px 0px 0px #000',
+      },
+      translate: {
+        boxShadowX: '6px',
+        boxShadowY: '6px',
+        reverseBoxShadowX: '-6px',
+        reverseBoxShadowY: '-6px',
+      },
+      fontWeight: {
+        base: '700',
+        heading: '900',
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
