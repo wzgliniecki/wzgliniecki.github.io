6b15b4f8161e72dc48525eb8d5e8a8bdb2bd57f1
Wojciech Zgliniecki <wojciech.zgliniecki@gmail.com>
2024-11-14 21:28:51 +0100

    Restyled navbar icons and minor changes.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,37 +6,37 @@ export default {
   theme: {
     extend: {
       colors: {
-        main: '#88aaee',
-        mainAccent: '#4d80e6', // not needed for shadcn components
-        overlay: 'rgba(0,0,0,0.8)', // background color overlay for alert dialogs, modals, etc.
-  
+        main: "#88aaee",
+        mainAccent: "#4d80e6", // not needed for shadcn components
+        overlay: "rgba(0,0,0,0.8)", // background color overlay for alert dialogs, modals, etc.
+
         // light mode
-        bg: '#dfe5f2',
-        text: '#000',
-        border: '#000',
-  
+        bg: "#dfe5f2",
+        text: "#000",
+        border: "#000",
+
         // dark mode
-        darkBg: '#272933',
-        darkText: '#eeefe9',
-        darkBorder: '#000',
-        secondaryBlack: '#212121', // opposite of plain white, not used pitch black because borders and box-shadows are that color 
+        darkBg: "#272933",
+        darkText: "#eeefe9",
+        darkBorder: "#000",
+        secondaryBlack: "#212121", // opposite of plain white, not used pitch black because borders and box-shadows are that color
       },
       borderRadius: {
-        base: '19px'
+        base: "19px",
       },
       boxShadow: {
-        light: '6px 6px 0px 0px #000',
-        dark: '6px 6px 0px 0px #000',
+        light: "6px 6px 0px 0px #000",
+        dark: "6px 6px 0px 0px #000",
       },
       translate: {
-        boxShadowX: '6px',
-        boxShadowY: '6px',
-        reverseBoxShadowX: '-6px',
-        reverseBoxShadowY: '-6px',
+        boxShadowX: "6px",
+        boxShadowY: "6px",
+        reverseBoxShadowX: "-6px",
+        reverseBoxShadowY: "-6px",
       },
       fontWeight: {
-        base: '700',
-        heading: '900',
+        base: "700",
+        heading: "900",
       },
     },
   },
